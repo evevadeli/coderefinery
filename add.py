8b3136dd0b758dea0a3ecd7cb2280c80f68a70a3
Eveline <75481340+evevadeli@users.noreply.github.com>
2022-12-13 11:49:00 +0100

    Revert "Hello"

--- add.py
+++
@@ -1,4 +0,0 @@
-# a function to add anything together
-def adding(a,b):
-    '''Add two objects together'''
-    return(a+b)
--- multiply.py
+++
@@ -1,4 +0,0 @@
-# function to multipy two things
-def multiply(a,b):
-    ```Multiply two objects```
-    return(a*b)
